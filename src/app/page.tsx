69081d443351f8566de524bb11e4d9ebbff3c304
eduardo <thebinarygardener@gmail.com>
2024-06-19 17:13:08 -0500

    checkpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import {theme} from "@/theme";
 import {FloraThemeProvider, BGLandingNav, BrowserOnlyFadeIn, FloraGlobalStyle} from "@/app/client";
 
 export default function Home() {
+
+
     return (
         <FloraThemeProvider theme={theme}>
             <FloraGlobalStyle/>
             <main>
                 <BrowserOnlyFadeIn>
-                    <BGLandingNav
-                        fullPageLandingProps={{imagePath: "test.jpg", title: "Binary Garden Documentation"}}/>
+                    <BGLandingNav imagePath={"test.jpg"} title={"Binary Garden Documentation"} buttonInfo={[]}>
+                        <div>children are here</div>
+                    </BGLandingNav>
                 </BrowserOnlyFadeIn>
             </main>
         </FloraThemeProvider>
